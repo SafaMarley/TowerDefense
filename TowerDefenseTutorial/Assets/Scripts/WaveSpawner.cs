7af38ca3c75625faf96d9dad55b64102baca7388
SafaMarley <safaktrcgl@gmail.com>
2021-09-07 16:53:15 +0300

    Episode 3&4 Done

---
+++ TowerDefenseTutorial/Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform enemyPrefab;
+    public Transform startPoint;
+    public float timeBetweenSpawns = 5f;
+    public Text countdownText;
+    
+    private float countdown = 2f;
+    private int waveNumber = 0;
+
+    private void Update()
+    {
+        if (countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenSpawns;
+        }
+
+        countdown -= Time.deltaTime;
+        countdownText.text = Mathf.Ceil(countdown).ToString();
+    }
+
+    IEnumerator SpawnWave()
+    {
+        waveNumber++;
+        for (int i = 0; i < waveNumber; i++)
+        {
+            Instantiate(enemyPrefab, startPoint.transform.position, enemyPrefab.transform.rotation);
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+}
---
+++ TowerDefenseTutorial/Assets/Turret.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    private Transform target;
+    
+    public float range = 15f;
+    public string enemyTag = "Enemy";
+    public Transform rotationPart;
+    public float turnSpeed = 10f;
+
+    private void Start()
+    {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+    }
+
+    void UpdateTarget ()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+        
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceFromEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distanceFromEnemy < shortestDistance)
+            {
+                shortestDistance = distanceFromEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if (shortestDistance <= range && nearestEnemy != null)
+        {
+            target = nearestEnemy.transform;
+        }
+        else
+        {
+            target = null;
+        }
+    }
+
+    private void Update()
+    {
+        if (target != null)
+        {
+            Vector3 direction = target.transform.position - transform.position;
+            Quaternion look = Quaternion.LookRotation(direction);
+            Vector3 lookRotation = Quaternion.Lerp(rotationPart.rotation, look, Time.deltaTime * turnSpeed).eulerAngles;
+
+            rotationPart.rotation = Quaternion.Euler(0f, lookRotation.y, 0f);
+        }
+    }
+    
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
