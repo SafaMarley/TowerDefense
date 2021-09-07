8ccbd83dbdc509f2293a5aae8dab5654f3357a60
SafaMarley <safaktrcgl@gmail.com>
2021-09-07 14:12:23 +0300

    episode1 & 2 Done

---
+++ TowerDefenseTutorial/Assets/Scripts/Enemy.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 10f;
+    private Transform target;
+    private int wavepointIndex = 0;
+
+    private void Start()
+    {
+        target = WayPoints.points[wavepointIndex];
+    }
+
+    private void Update()
+    {
+        Vector3 dir = (target.position - transform.position).normalized;
+        transform.Translate(dir * speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.2f)
+        {
+            GetNextWayPoint();
+        }
+    }
+
+    void GetNextWayPoint()
+    {
+        if (wavepointIndex >= WayPoints.points.Length - 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            wavepointIndex++;
+            target = WayPoints.points[wavepointIndex];
+        }
+    }
+}
